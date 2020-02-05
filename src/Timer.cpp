7625821371e911ef0126692a40d9e414040530fc
GALES Quentin <qgales@taghazout-vm.pedago.ipb.fr>
2020-02-05 11:24:05 +0100

    MAJ b_transport_pcm

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -33,27 +33,8 @@ void Timer::b_transport_bus(tlm_generic_payload& trans, sc_time& delay)
     }
   }
 
-  void Timer::b_transport_pcm(tlm_generic_payload& trans, sc_time& delay)
+  void Timer::b_transport_pcm(struct& clocks)
   {
-    tlm_command      cmd = trans.get_command();
-    sc_dt::uint64    adr = trans.get_address() / 4;
-    unsigned char*   ptr = trans.get_data_ptr();
-    unsigned int     len = trans.get_data_length();
-    unsigned char*   byt = trans.get_byte_enable_ptr();
-    unsigned int     wid = trans.get_streaming_width();
-
-    if (adr >= MEMORY_MAX_SIZE || byt != 0 || (adr + len) >= MEMORY_MAX_SIZE) {
-      SC_REPORT_ERROR("TLM-2", "Timer does not support given generic payload transaction");
-    }
-
-    if (cmd == TLM_READ_COMMAND) {
-      memcpy(ptr, &mem[adr], len);
-    }
-    else if (cmd == TLM_WRITE_COMMAND) {
-      memcpy(&mem[adr], ptr, len);
-    }
-  }
-
-  trans.set_response_status(TLM_OK_RESPONSE)
-
+	setInputClocks(clocks.MCLK, clocks.SCLK);
+	
 }
