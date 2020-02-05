4593a06fd7f44c81644aa03b351f5c76afeea0b3
David <david.devant@live.fr>
2020-02-05 23:24:38 +0100

    Add Tools.h

---
+++ src/Tools.h
@@ -0,0 +1,12 @@
+#ifndef _TOOLS_H_
+#define _TOOLS_H_
+
+// MACROS
+#define is_in_range(value, start, len)  ((value >= start) && (value < (start + len)))
+
+// CONSTANTS
+#define KILO	(1000)
+#define MEGA	(KILO * KILO)
+#define GIGA	(KILO * MEGA)
+
+#endif /* _TOOLS_H_ */
