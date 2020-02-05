fd5edcec169acc709e0ff9a85560d8745a3911fb
Fefee <ophelie.amorosi90@gmail.com>
2020-02-05 08:39:23 +0100

    modif de timer.cpp

---
+++ src/Timer.cpp
@@ -0,0 +1,8 @@
+#include "Timer.h"
+
+Timer::Timer(sc_module_name name) : sc_module(name){
+  channel_1 = new channel("channel");
+  channel_2 = new channel("channel");
+  channel_2 = new channel("channel");
+	
+}
